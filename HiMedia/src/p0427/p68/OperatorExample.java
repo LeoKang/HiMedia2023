dbb3775d6bbfb773f21aa45c540ae203f1533076
Class01 <Class01@DESKTOP-9H6B9C5>
2023-04-27 14:54:54 +0900

    0427 commit

---
+++ HiMedia/src/p0427/p68/OperatorExample.java
@@ -0,0 +1,15 @@
+package p0427.p68;
+
+public class OperatorExample {
+	public static void main(String[] args) {
+		boolean bPlay = true;
+
+		System.out.println(bPlay);
+
+		bPlay = !bPlay;
+		System.out.println(bPlay);
+
+		bPlay = !bPlay;
+		System.out.println(bPlay);
+	}
+}
