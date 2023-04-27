d0000406ed3d1cf3aed414ab89e958c91f4b40b3
Class01 <Class01@DESKTOP-9H6B9C5>
2023-04-27 15:19:20 +0900

    0427 - p74

---
+++ HiMedia/src/p0427/p73/OperatorExample.java
@@ -0,0 +1,26 @@
+package p0427.p73;
+
+public class OperatorExample {
+	public static void main(String[] args) {
+		byte a = 10;
+		int b = 5;
+		int c = 7;
+		double d = 4.8;
+		float f = 5.2f;
+
+		int result1 = a + b;
+//		byte result3 = a + b;
+		double result2 = c + d;
+//		int result4 = c + d;
+		float result5 = c + f;
+//		int result6 = c + f;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+
+		System.out.println(result1);
+		System.out.println(result2);
+	}
+}
