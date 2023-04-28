af40de5476dfbe9b7802e98a680d7f0e517e79ec
Class01 <Class01@DESKTOP-9H6B9C5>
2023-04-28 15:53:50 +0900

    수업자료 part2-11page

---
+++ HiMedia/0428/part2/p8/IfNestedExample.java
@@ -0,0 +1,28 @@
+package part2.p8;
+
+import java.util.Scanner;
+
+public class IfNestedExample {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+//		int score = 84;
+		int score = scan.nextInt();
+		scan.close();
+
+		if (score >= 90) {
+			System.out.print("A");
+			if (score >= 95) {
+				System.out.print("+");
+			} else {
+				System.out.print("-");
+			}
+		} else {
+			System.out.print("B");
+			if (score >= 85) {
+				System.out.print("+");
+			} else {
+				System.out.print("-");
+			}
+		}
+	}
+}
