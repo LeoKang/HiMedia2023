c55afe0c6feeb3268097c5c5f3e33fa1c9169849
Class01 <Class01@DESKTOP-9H6B9C5>
2023-05-25 11:33:45 +0900

    DmbCellPhone

--- HiMedia/0515/sec03/exam01/Car.java
+++ HiMedia/0515/sec03/exam01/Car.java
@@ -1,5 +1,19 @@
 package sec03.exam01;
 
 public class Car {
+//	Car() {
+//		
+//	}
+	
+	Car(String color, int cc) {
 
+	}
+
+	public void method() {
+		System.out.println("method()");
+	}
+
+	public void method1(int a) {
+		System.out.println(a);
+	}
 }
