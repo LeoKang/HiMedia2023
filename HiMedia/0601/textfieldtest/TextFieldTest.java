896fb591e85666fb4e9a3a39d02bbf287c9cbe49
Class01 <Class01@DESKTOP-9H6B9C5>
2023-06-01 14:22:51 +0900

    TextFieldTest - 로그인 성공 시, 새로운 Frame 띄우기

--- HiMedia/0601/textfieldtest/TextFieldTest.java
+++ HiMedia/0601/textfieldtest/TextFieldTest.java
@@ -5,17 +5,24 @@ import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Label;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TextFieldTest {
-	private Frame f;
+public class TextFieldTest implements ActionListener {
+	private Frame f, f2;
 	private TextField id, pwd, msg;
 	private Button btn;
+	private final String STRING_ID = "himedia";
+	private final String STRING_PWD = "1111";
 
 	public TextFieldTest() {
 		f = new Frame("Login");
 		f.setSize(400, 120);
 		f.setLayout(new FlowLayout());
 
+		f2 = new Frame("Main");
+		f2.setSize(500, 500);
+		
 		Label lid = new Label("ID : ", Label.RIGHT);
 		Label lpwd = new Label("Password : ", Label.RIGHT);
 
@@ -24,8 +31,10 @@ public class TextFieldTest {
 		pwd.setEchoChar('*');
 
 		btn = new Button("Login");
+		btn.addActionListener(this);
 
 		msg = new TextField(40);
+		msg.setEditable(false);
 
 		f.add(lid);
 		f.add(id);
@@ -38,5 +47,20 @@ public class TextFieldTest {
 
 	public static void main(String[] args) {
 		TextFieldTest t = new TextFieldTest();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("[" + id.getText() + "]");
+		System.out.println("[" + pwd.getText() + "]");
+		
+		if(id.getText().equals(STRING_ID) && pwd.getText().equals(STRING_PWD)) {
+//			System.out.println("로그인 성공");
+			msg.setText("로그인 성공");
+			f2.setVisible(true);
+		}else {
+			System.out.println("로그인 실패");
+			msg.setText("로그인 실패");
+		}
 	}
 }
