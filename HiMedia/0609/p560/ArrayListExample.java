2ba8052e4820c1e3daccac0a6e5161284c31e0c9
Class01 <Class01@DESKTOP-9H6B9C5>
2023-06-13 10:04:37 +0900

    Coin Class

--- HiMedia/0609/p560/ArrayListExample.java
+++ HiMedia/0609/p560/ArrayListExample.java
@@ -7,33 +7,42 @@ public class ArrayListExample {
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<String>();
 
-		list.add("Java");
-		list.add("JDBC");
-		list.add("Servlet/JSP");
-		list.add(2, "Database");
-		list.add("iBATIS");
+		String[] str = { "Java", "JDBC", "Servlet/JSP", "Database", "iBATIS" };
 
-		int size = list.size();
-		System.out.println("총 객체 수 : " + size);
-		System.out.println();
-
-		String skill = list.get(2);
-		System.out.println("2 : " + skill);
-		System.out.println();
-
-		for (int i = 0; i < list.size(); i++) {
-			String str = list.get(i);
-			System.out.println(i + ":" + str);
+		for (int i = 0; i < str.length; i++) {
+			list.add(str[i]);
 		}
-		System.out.println();
 
-		list.remove(2);
-		list.remove(2);
-		list.remove("iBATIS");
+//		list.add("Java");
+//		list.add("JDBC");
+//		list.add("Servlet/JSP");
+//		list.add(2, "Database");
+//		list.add("iBATIS");
+//		list.add("Java");
+//		
+//		list.remove("Java");
 
-		for (int i = 0; i < list.size(); i++) {
-			String str = list.get(i);
-			System.out.println(i + ":" + str);
-		}
+//		int size = list.size();
+//		System.out.println("총 객체 수 : " + size);
+//		System.out.println();
+//
+//		String skill = list.get(2);
+//		System.out.println("2 : " + skill);
+//		System.out.println();
+//
+//		for (int i = 0; i < list.size(); i++) {
+//			String str = list.get(i);
+//			System.out.println(i + ":" + str);
+//		}
+//		System.out.println();
+//
+//		list.remove(2);
+//		list.remove(2);
+//		list.remove("iBATIS");
+
+//		for (int i = 0; i < list.size(); i++) {
+//			String str = list.get(i);
+//			System.out.println(i + ":" + str);
+//		}
 	}
 }
---
+++ HiMedia/0613/stack/coin/Coin.java
@@ -0,0 +1,13 @@
+package stack.coin;
+
+public class Coin {
+	private int c;
+
+	public Coin(int c) {
+		this.c = c;
+	}
+
+	public int getValue() {
+		return c;
+	}
+}
